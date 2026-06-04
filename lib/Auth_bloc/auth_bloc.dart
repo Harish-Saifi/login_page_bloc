5369ce33b39189afc0b1b90f9ff1c901da24a8f4
Harish-Saifi <saifiharish32@gmail.com>
2026-06-04 12:47:42 +0530

    modified code to use some clean pattern in bloc class.

--- lib/Auth_bloc/auth_bloc.dart
+++ lib/Auth_bloc/auth_bloc.dart
@@ -1,37 +1,44 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'auth_states.dart';
 part 'auth_events.dart';
 
 class AuthBloc  extends Bloc<AuthEvents,AuthStates>{
     AuthBloc():super(AuthInitial()) {
-      on<AuthSubmittedEvent>((event,state)async{
-           String email = event.email;
-           String password = event.password;
-           var (emailError, passwordError) = validateInput(email, password);
-           if(emailError != null || passwordError != null){
-               return emit(AuthError(emailError, passwordError));
-           }
-           emit(AuthLoading());
-           await Future.delayed(Duration(seconds: 3));
-           String msg = checkInput(email, password);
-           return emit(AuthResult(msg: msg,email: email));
-      });
-      on<AuthLogoutEvent>((event,state) async{
-        emit(AuthLoading());
-        try{
-          await Future.delayed(Duration(seconds: 2));
-          emit(AuthInitial());
-        }catch(e){
-          emit(AuthException(e.toString()));
-        }
-      });
+      on<AuthSubmittedEvent>(_onAuthSubmittedEvent);
+      on<AuthLogoutEvent>(_onAuthLogoutEvent);
     }
 
 
-    (String?, String?) validateInput(String email,String password){
+    @override
+    void onChange(Change<AuthStates> change) {
+      super.onChange(change);
+      debugPrint("Change of state : $change");
+    }
 
-      String? emailErr = null;
-      String? psswdErr = null;
+    @override
+  void onTransition(Transition<AuthEvents, AuthStates> transition) {
+    super.onTransition(transition);
+    debugPrint("Transition : $transition");
+  }
+
+    Future<void> _onAuthSubmittedEvent(AuthSubmittedEvent event,Emitter<AuthStates> emit) async{
+      String email = event.email;
+      String password = event.password;
+      var (emailError, passwordError) = _validateInput(email, password);
+      if(emailError != null || passwordError != null){
+        return emit(AuthError(emailError, passwordError));
+      }
+      emit(AuthLoading());
+      await Future.delayed(Duration(seconds: 3));
+      String msg = _checkInput(email, password);
+      return emit(AuthResult(msg: msg,email: email));
+    }
+
+    (String?, String?) _validateInput(String email,String password){
+
+      String? emailErr;
+      String? psswdErr;
       if(email.isEmpty){
            emailErr = "*Email is required.";
       }
@@ -47,14 +54,22 @@ class AuthBloc  extends Bloc<AuthEvents,AuthStates>{
       return (emailErr,psswdErr);
     }
 
-
-    String checkInput(String email, String password){
+    String _checkInput(String email, String password){
          if(email == "abc@yopmail.com" && password == "123abc"){
             return "Login Successful";
          }
          return "Invalid Credentials";
     }
 
+    Future<void> _onAuthLogoutEvent (AuthLogoutEvent event, Emitter<AuthStates> emit) async{
+      emit(AuthLoading());
+      try{
+        await Future.delayed(Duration(seconds: 2));
+        emit(AuthInitial());
+      }catch(e){
+        emit(AuthException(e.toString()));
+      }
+    }
 
 
 }
